5dba201fc759e05d91bda9514108dc4a8b099d71
Vikranth <vikranthyadlapalli123@gmail.com>
2026-02-11 11:48:05 +0530

    GoodReads First Commit

---
+++ TodaysDate/src/main/java/com/practice/spring/TodaysDate/TodaysDateApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spring.TodaysDate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaysDateApplication.class, args);
+	}
+
+}
---
+++ TodaysDate/src/test/java/com/practice/spring/TodaysDate/TodaysDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring.TodaysDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
